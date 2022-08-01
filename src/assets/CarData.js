2a4f5e5f12a5cf92b7f52c05dacf40063022598f
Islam Bahnas <islambahnas1@gmail.com>
2022-08-01 04:53:26 +0200

    Add data for test

--- src/assets/CarData.js
+++
@@ -1,7 +0,0 @@
-export const car = {
-  user: "User.first",
-  name: "toyota",
-  description: "best for afford",
-  picture: "https://toyota.com.eg/storage/17011/CORO1811-1001.jpg",
-  price: 100.00
-};
