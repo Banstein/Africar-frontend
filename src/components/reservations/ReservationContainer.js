5af7d65eb3f6dac5ae3909f4248a23d02bc40d6f
Nicholas Mutale <nicomutale@gmail.com>
2022-08-01 21:58:29 +0200

    Update reservation container

---
+++ src/components/reservations/ReservationContainer.js
@@ -0,0 +1,44 @@
+import ReservedItem from './ReservedItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { openModal } from '../features/modal/modalSlice';
+
+const ReservedContainer = () => {
+  const dispatch = useDispatch();
+  const {ReservedItem, total, amount } = useSelector((store) => store.reservation);
+
+  if (amount < 1) {
+    return (
+      <section className='car'>
+        <header>
+          <h2>Reserved</h2>
+          <h4 className='empty-car'>No Car Reserved Yet</h4>
+        </header>
+      </section>
+    );
+  }
+
+  return (
+    <section className='car'>
+      <header>
+        <h2>Reserved Car List</h2>
+      </header>
+      <div>
+        {ReservedItem.map((item) => {
+          return <CarItem key={item.id} {...item} />;
+        })}
+      </div>
+      <footer>
+        <hr />
+        <div className='car-total'>
+          <h4>
+            total <span>${total.toFixed(2)}</span>
+          </h4>
+        </div>
+        <button className='btn clear-btn' onClick={() => dispatch(openModal())}>
+          RESERVATIONS
+        </button>
+      </footer>
+    </section>
+  );
+};
+export default ReservedContainer;
