394fbbb8ca93a1a3932cf4357a631e6a3593d9bf
Pascal <pascal@Pascals-MacBook-Pro.local>
2022-08-02 19:10:35 +0200

    Implement the car list page based on the design

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -15,91 +15,90 @@ function Signup() {
       <body class="h-full">
       ```
     */}
-      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 content-center">
-        <div className="max-w-md w-full space-y-8  py-4 px-6">
+      <div className='min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 content-center'>
+        <div className='max-w-md w-full space-y-8  py-4 px-6'>
           <div>
-            <img className="mx-auto h-20 w-auto" src={logo} alt="Workflow" />
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            <img className='mx-auto h-20 w-auto' src={logo} alt='Workflow' />
+            <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
               Create your account
             </h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
+            <p className='mt-2 text-center text-sm text-gray-600'>
               Kindly,
-              {' '}
               <a
-                href="https://www.microverse.org/info/terms#:~:text=Microverse%20does%20not%20knowingly%20collect,individual%20and%20is%20non%2Dtransferrable."
-                className="font-medium text-lime-600 hover:text-lime-700"
+                href='https://www.microverse.org/info/terms#:~:text=Microverse%20does%20not%20knowingly%20collect,individual%20and%20is%20non%2Dtransferrable.'
+                className='font-medium text-lime-600 hover:text-lime-700'
               >
                 read our terms and conditions to continue!
               </a>
             </p>
           </div>
-          <form className="mt-8 space-y-6 login-form" action="#" method="POST">
-            <input type="hidden" name="remember" defaultValue="true" />
-            <div className="rounded-md shadow-sm -space-y-px">
+          <form className='mt-8 space-y-6 login-form' action='#' method='POST'>
+            <input type='hidden' name='remember' defaultValue='true' />
+            <div className='rounded-md shadow-sm -space-y-px'>
               <div>
-                <label htmlFor="email-address" className="sr-only">
+                <label htmlFor='email-address' className='sr-only'>
                   Email address
                 </label>
                 <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
+                  id='email-address'
+                  name='email'
+                  type='email'
+                  autoComplete='email'
                   required
-                  className="appearance-none  relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-green-500 focus:border-gray-500 focus:z-10 sm:text-sm"
-                  placeholder="Email address"
+                  className='appearance-none  relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-green-500 focus:border-gray-500 focus:z-10 sm:text-sm'
+                  placeholder='Email address'
                 />
               </div>
               <div>
-                <label htmlFor="password" className="sr-only">
+                <label htmlFor='password' className='sr-only'>
                   Password
                 </label>
                 <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
+                  id='password'
+                  name='password'
+                  type='password'
+                  autoComplete='current-password'
                   required
-                  className="appearance-none  relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-gray-500 focus:border-gray-500 focus:z-10 sm:text-sm"
-                  placeholder="Password"
+                  className='appearance-none  relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-gray-500 focus:border-gray-500 focus:z-10 sm:text-sm'
+                  placeholder='Password'
                 />
               </div>
               <div>
-                <label htmlFor="password" className="sr-only">
+                <label htmlFor='password' className='sr-only'>
                   Confirm Password
                 </label>
                 <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
+                  id='password'
+                  name='password'
+                  type='password'
+                  autoComplete='current-password'
                   required
-                  className="appearance-none  relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-gray-500 focus:border-gray-500 focus:z-10 sm:text-sm"
-                  placeholder="Confirm Password"
+                  className='appearance-none  relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-gray-500 focus:border-gray-500 focus:z-10 sm:text-sm'
+                  placeholder='Confirm Password'
                 />
               </div>
             </div>
 
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
+            <div className='flex items-center justify-between'>
+              <div className='flex items-center'>
                 <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 text-gray-700 focus:ring-indigo-500 border-gray-300 rounded"
+                  id='remember-me'
+                  name='remember-me'
+                  type='checkbox'
+                  className='h-4 w-4 text-gray-700 focus:ring-indigo-500 border-gray-300 rounded'
                 />
                 <label
-                  htmlFor="remember-me"
-                  className="ml-2 block text-sm text-gray-900"
+                  htmlFor='remember-me'
+                  className='ml-2 block text-sm text-gray-900'
                 >
                   Remember me
                 </label>
               </div>
 
-              <div className="text-sm">
+              <div className='text-sm'>
                 <a
-                  href="#"
-                  className="font-medium text-blue-700 hover:text-gray-300"
+                  href='#'
+                  className='font-medium text-blue-700 hover:text-gray-300'
                 >
                   Forgot your password?
                 </a>
@@ -108,23 +107,23 @@ function Signup() {
 
             <div>
               <button
-                type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                type='submit'
+                className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
               >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                <span className='absolute left-0 inset-y-0 flex items-center pl-3'>
                   <LockClosedIcon
-                    className="h-5 w-5 text-white group-hover:text-green-600"
-                    aria-hidden="true"
+                    className='h-5 w-5 text-white group-hover:text-green-600'
+                    aria-hidden='true'
                   />
                 </span>
                 Signup
               </button>
 
-              <div className="text-sm">
-                <Link className="" to="/login">
+              <div className='text-sm'>
+                <Link className='' to='/login'>
                   <p
-                    href="#"
-                    className="font-medium text-blue-700 hover:text-gray-300 link-to-signup"
+                    href='#'
+                    className='font-medium text-blue-700 hover:text-gray-300 link-to-signup'
                   >
                     already a member? Login
                   </p>
