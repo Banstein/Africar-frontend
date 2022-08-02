7117a959fc2b75f653bebdebd59d4f1ce0617822
Islam Bahnas <islambahnas1@gmail.com>
2022-08-02 04:15:48 +0200

    Refactoring the code, delete unnecessary codes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import Navbar from './components/Navbar';
 import CarContainer from './components/CarContainer';
 import { useDispatch, useSelector } from 'react-redux';
-import { calculateTotals, getCarItems } from './features/car/carSlice';
+import { getCarItems } from './features/car/carSlice';
 import { useEffect } from 'react';
 import Modal from './components/Modal';
 function App() {
-  const { carItems, isLoading } = useSelector((store) => store.car);
+  const { isLoading } = useSelector((store) => store.car);
   const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(calculateTotals());
-  }, [carItems]);
-
-  useEffect(() => {
-    dispatch(getCarItems('random'));
+    dispatch(getCarItems());
   }, []);
 
   if (isLoading) {
