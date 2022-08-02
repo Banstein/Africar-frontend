ac259439ed81396033268dcc5ee3f0b634d641e0
Nicholas Mutale <nicomutale@gmail.com>
2022-08-02 19:37:36 +0200

    resolve merge conflicts in index js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-<<<<<<< HEAD
-=======
 import './app.css';
->>>>>>> 627a70827361a847c2e6a3fe6e43faaa95ae2018
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './store';
