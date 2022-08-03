be0f9de4dfb9184037600a838fe530ec42747ecf
Islam Bahnas <islambahnas1@gmail.com>
2022-08-03 16:50:45 +0200

    fix linter errors 1

--- src/components/carsCards/CardAudi.js
+++ src/components/carsCards/CardAudi.js
@@ -4,23 +4,23 @@ import Car from '../../assets/race.png';
 function CardAudi() {
   return (
     <div>
-      <div className='overflow-hidden rounded shadow-lg my-card'>
-        <img src={Car} className='w-full' />
-        <div className='px-4 py-2'>
-          <div className='mb-1 text-sm font-bold'>Race double gear 4X4</div>
-          <span className='text-sm text-gray-700'>
+      <div className="overflow-hidden rounded shadow-lg my-card">
+        <img src={Car} className="w-full" />
+        <div className="px-4 py-2">
+          <div className="mb-1 text-sm font-bold">Race double gear 4X4</div>
+          <span className="text-sm text-gray-700">
             City car. These range from compact and fuel-efficient city to
             eco-friendly model. Available in store with discount!
           </span>
         </div>
-        <div className='px-6 pt-4 pb-2'>
-          <span className='inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full'>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">
             Views
           </span>
-          <span className='inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full'>
+          <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">
             Details
           </span>
-          <span className='inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full'>
+          <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">
             Book
           </span>
         </div>
--- src/components/carsCards/CardBenz.js
+++ src/components/carsCards/CardBenz.js
@@ -4,23 +4,23 @@ import Car from '../../assets/benz.png';
 function CardBenz() {
   return (
     <div>
-      <div className='overflow-hidden rounded shadow-lg my-card'>
-        <img src={Car} className='w-full' />
-        <div className='px-4 py-2'>
-          <div className='mb-1 text-sm font-bold'>Mercedes benz new modal</div>
-          <span className='text-sm text-gray-700'>
+      <div className="overflow-hidden rounded shadow-lg my-card">
+        <img src={Car} className="w-full" />
+        <div className="px-4 py-2">
+          <div className="mb-1 text-sm font-bold">Mercedes benz new modal</div>
+          <span className="text-sm text-gray-700">
             Mercedes benz, Prestige You can choose from a wide range of luxury
             vehicles made by legendary manufacturers!
           </span>
         </div>
-        <div className='px-6 pt-4 pb-2'>
-          <span className='inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full'>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">
             Views
           </span>
-          <span className='inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full'>
+          <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">
             Details
           </span>
-          <span className='inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full'>
+          <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">
             Book
           </span>
         </div>
