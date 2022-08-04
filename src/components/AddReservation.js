4b1317c2823aa8f236596b37859142a69d67a151
Nicholas Mutale <nicomutale@gmail.com>
2022-08-04 10:40:13 +0200

    Add backround image style page

--- src/components/AddReservation.js
+++ src/components/AddReservation.js
@@ -1,100 +1,84 @@
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import axios from "axios";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const ReserveForm = () => {
-  const [carName, setCarName] = useState([]);
-  const user = useSelector(state => state.user);
+const ReservePage = () => {
+  const [names, setNames] = useState([]);
+  const user = useSelector((state) => state.user);
   useEffect(() => {
     (async () => {
-      const response = await fetch(`https://africar-premium.herokuapp.com/api/v1/cars/`);
-      const data = await response.json();
-      const carName = data.map((item) => ({
-        car_id: item.id,
-        car_name: item.name,
+      const res = await fetch('https://car-booking-premium.herokuapp.com/api/v1/cars');
+      const data = await res.json();
+      const names = data.map((el) => ({
+        car_id: el.id,
+        names: `${el.make} - ${el.names}`,
       }));
-      setCarName(carName);
-      }
-    )();
+      setNames(names);
+    })();
   }, []);
-  const [formSubmit, setFormSubmit] = useState(false);
-  const [formData, setFormData] = useState('Formsubmission failed.Enter all fileds');
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setFormSubmit(true);
-    setTimeout(() => { setFormSubmit(false); }, 3000);
-    if (e.target.city.value && e.target.car_id.value && e.target.user_id.value) {
-      axios.post(`https://africar-premium.herokuapp.com/api/v1/reservations/${user.id}`, {
-        city: e.target.city.value, 
-        car_id: e.target.car_id.value,
-        user_id: e.target.user_id.value,
-      })  
-      .then(() => {
-        setFormData('Formsubmission successful');
-      }
-      );
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [onFormSubmitMessage, setOnFormSubmitMessage] = useState('Form submission failed. Make sure to fill all the fields.');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFormSubmitted(true);
+    setTimeout(() => setFormSubmitted(false), 3000);
+    if (event.target.city.value && event.target.car_id.value && user_id) {
+      axios.post(`https://africar-premium.herokuapp.com/${user.id}/api/v1/reservations/`, {
+        city: event.target.city.value,
+        car_id: event.target.car_id.value,
+        user_id: user.id,
+      }).then(() => {
+        setOnFormSubmitMessage('Form submission succeeded.');
+      });
     }
-  }
+  };
   return (
-    <section className="h-full flex flex-col items-center justify-center">
-      <h2 className="text-center text-3xl font-bold">Reserve a car</h2>
-      <form className="w-full max-w-lg p-4" onSubmit={handleSubmit}>
-        <div className="flex flex-col">
-          <label htmlFor="city" className="block text-gray-700 text-sm font-bold mb-2">
-            City
-          </label>
-          <input
-
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="city"
-            type="text"
-            placeholder="Enter your city"
-            required
-          />
+    <section className="fixed top-0 w-full h-full md:pl-[20vw] bg-[url('/src/assets/backgroundReserveCar.jpg')] bg-right bg-no-repeat bg-100%">
+      <div className="h-full  bg-[#81B622]/70 px-20 flex flex-col justify-center">
+        <div className="w-full h-[50%] text-center flex flex-col justify-center items-center gap-[2rem]">
+          <h1 className="font-bold text-white text-3xl md:text-5xl md:mb-5">
+            MAKE YOUR RESERVATION
+          </h1>
+          {formSubmitted && (
+            <div className="bg-#81B622-100 border border-#81B622-400 text-#81B622-700 px-4 py-3 rounded fixed top-4" role="alert">
+              <span className="block sm:inline">{onFormSubmitMessage}</span>
+            </div>
+          )}
+          <form onSubmit={handleSubmit} className="w-full h-full text-center flex flex-col items-center gap-[2rem] md:flex-row lg:w-[70%] md:h-auto">
+            <div className="w-full md:w-auto md:h-full">
+              <label htmlFor="city" className="text-white text-2xl md:text-3xl">
+                City
+              </label>
+              <input
+                type="text"
+                name="city"
+                id="city"
+                className="bg-#81B622-100 border border-#81B622-400 text-#81B622-700 px-4 py-3 rounded"
+                placeholder="Enter your city"
+                required
+              />
+            </div>
+            <div className="w-full md:w-auto md:h-full">
+              <label htmlFor="car_id" className="text-white text-2xl md:text-3xl">
+                Car
+              </label>
+              <select name="car_id" id="car_id" className="bg-#81B622-100 border border-#81B622-400 text-#81B622-700 px-4 py-3 rounded">
+                {names.map((el) => (
+                  <option key={el.car_id} value={el.car_id}>
+                    {el.names}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="w-full md:w-auto md:h-full">
+              <button type="submit" className="bg-#81B622-100 border border-#81B622-400 text-#81B622-700 px-4 py-3 rounded">
+                Reserve
+              </button>
+            </div>
+          </form>
         </div>
-        <div className="flex flex-col">
-          <label htmlFor="date" className="block text-gray-700 text-sm font-bold mb-2">
-            Date
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-
-            id="date"
-            type="date"
-            placeholder="Enter your date"
-            required
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="car_id" className="block text-gray-700 text-sm font-bold mb-2">
-            Car
-          </label>
-          <select
-
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="car_id"
-            required
-          >
-            <option value="">Select a car</option>
-            {carName.map((item) => (
-              <option key={item.car_id} value={item.car_id}>
-                {item.car_name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="flex flex-col">
-          <button
-
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-      {formSubmit && <p className="text-center text-gray-700 text-sm">{formData}</p>}
+      </div>
     </section>
   );
-};
-export default ReserveForm;
+}
+export default ReservePage;
