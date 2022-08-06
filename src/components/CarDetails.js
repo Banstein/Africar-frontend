fd06144850e50e7144faf8c99dd40260499807e2
Nicholas Mutale <nicomutale@gmail.com>
2022-08-06 06:34:22 +0200

    Remove commented code

--- src/components/CarDetails.js
+++ src/components/CarDetails.js
@@ -1,13 +1,9 @@
-// /* eslint-disable react/prop-types */
-// import { useDispatch } from 'react-redux';
-// import { getCarItems } from '../features/car/carSlice';
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 const CarDetails = () => {
   const { id } = useParams();
-  // console.log(id);
   const [car, setCar] = useState({});
   const fetchCar = async () => {
     const res = await fetch(`https://africar-premium.herokuapp.com/api/v1/cars/${id}`);
